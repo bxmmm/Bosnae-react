b52bf63bfa326389f782bcbf5d7a08666e101324
bxmmm <benjaminxh@gmail.com>
2017-01-16 00:15:47 +0100

    fix

--- server.js
+++ server.js
@@ -7,8 +7,15 @@ var app = express();
 // Database connect
 mongoose.connect(process.env.BAZA);
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Route for GET API request to show data
-app.get('/api/articles', function(req, res){
+app.get('/api/articles', function(req, res, next){
 	Article.find({}, function(err, foundArticles){
         if (err) {
             console.log(err);
