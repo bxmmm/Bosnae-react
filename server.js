a67b20457765d7ceb6b48384abbe9b448f02cd89
bxmmm <benjaminxh@gmail.com>
2017-01-18 23:31:29 +0100

    herokufix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var Article = require('./models/article');
 var Link = require('./models/link');
-var BAZA = require('./baza');
+// var BAZA = require('./baza');
 
 var app = express();
 
